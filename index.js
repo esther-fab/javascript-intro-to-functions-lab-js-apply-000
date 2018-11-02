803f679d60959adf38fd87ee860b1c072a118ad0
Esther Fab <esther.y.fab@icloud.com>
2018-11-02 01:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string == .toLowerCase()) {
+  if (string == ) {
    return "I can\'t hear you";
   }
   else if (string == "I love you Grandma") {
