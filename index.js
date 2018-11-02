9154b642caa4d6a6c0db2825073620cf42ae2dd8
Esther Fab <esther.y.fab@icloud.com>
2018-11-02 00:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string){
   if (string == lowercase) {
    return "I can\'t hear you";
   }
-  else if (string == "uppercase") {
+  else if (string == "I love you Grandma") {
     return "I love you too"
   }
-  else {
+  else (string == uppercase) {
     return ""
   }
   }
